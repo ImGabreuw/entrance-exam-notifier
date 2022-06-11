f43566e28608d71acd98712e189fad9f353ffc7a
ImGabreuw <imgabreuw.dev@gmail.com>
2022-06-11 19:38:42 -0300

    feat: add 'findAll' method in 'UserRepository'

--- src/main/java/br/com/gabreuw/entranceexamnotifier/domain/ports/repository/UserRepository.java
+++ src/main/java/br/com/gabreuw/entranceexamnotifier/domain/ports/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package br.com.gabreuw.entranceexamnotifier.domain.ports.repository;
 
 import br.com.gabreuw.entranceexamnotifier.domain.entities.authentication.User;
+import br.com.gabreuw.entranceexamnotifier.shared.pagination.PageInfo;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository {
+
+    List<User> findAll(PageInfo pageInfo);
 
     Optional<User> findByEmail(String email);
 
