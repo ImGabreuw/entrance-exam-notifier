b135f1245ebd37f28f2420e321c1e3a2dc678aaf
ImGabreuw <imgabreuw.dev@gmail.com>
2022-06-11 19:38:59 -0300

    feat: add 'NotifyEntranceExamRegistrationDeadlineUseCase'

--- src/main/java/br/com/gabreuw/entranceexamnotifier/domain/usecases/entrance/exam/NotifyEntranceExamRegistrationDeadlineUseCase.java
+++ src/main/java/br/com/gabreuw/entranceexamnotifier/domain/usecases/entrance/exam/NotifyEntranceExamRegistrationDeadlineUseCase.java
@@ -1,18 +1,62 @@
 package br.com.gabreuw.entranceexamnotifier.domain.usecases.entrance.exam;
 
+import br.com.gabreuw.entranceexamnotifier.domain.entities.authentication.User;
+import br.com.gabreuw.entranceexamnotifier.domain.entities.email.Email;
+import br.com.gabreuw.entranceexamnotifier.domain.ports.email.EmailService;
 import br.com.gabreuw.entranceexamnotifier.domain.ports.repository.EntranceExamRepository;
+import br.com.gabreuw.entranceexamnotifier.domain.ports.repository.UserRepository;
 import br.com.gabreuw.entranceexamnotifier.domain.usecases.UseCase;
+import br.com.gabreuw.entranceexamnotifier.shared.date.DateHelper;
+import br.com.gabreuw.entranceexamnotifier.shared.pagination.PageInfo;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 
+@Log4j2
 public record NotifyEntranceExamRegistrationDeadlineUseCase(
-        EntranceExamRepository entranceExamRepository
+        EntranceExamRepository entranceExamRepository,
+        UserRepository userRepository,
+        EmailService emailService,
+        @Value("${spring.mail.username}")
+        String sender
 ) implements UseCase<NotifyEntranceExamRegistrationDeadlineUseCase.InputValues, NotifyEntranceExamRegistrationDeadlineUseCase.OutputValues> {
 
     @Override
     public NotifyEntranceExamRegistrationDeadlineUseCase.OutputValues execute(NotifyEntranceExamRegistrationDeadlineUseCase.InputValues input) {
-        return null;
+        var pageInfo = input.pageInfo();
+
+        var entranceExamResult = entranceExamRepository.findByRegistrationDeadlineNearToReach(pageInfo);
+
+        var emails = userRepository
+                .findAll(pageInfo)
+                .stream()
+                .map(User::getEmail)
+                .map(br.com.gabreuw.entranceexamnotifier.domain.entities.authentication.value.objects.Email::value)
+                .toArray(String[]::new);
+
+        entranceExamResult.stream()
+                .map(entranceExam -> Email.Builder
+                        .builder()
+                        .from(sender)
+                        .to(emails)
+                        .subject("Prazo de inscrição para a faculdade %s está acabando...".formatted(entranceExam.getCollegeName()))
+                        .text(
+                                """
+                                O prazo de inscrição para %s é dia %s.
+                                
+                                Para realizar a sua inscrição acesse:
+                                %s
+                                """.formatted(entranceExam.getCollegeName(), DateHelper.format(entranceExam.getRegistrationDeadline()), entranceExam.getWebsite())
+                        )
+                        .build()
+                )
+                .forEach(emailService::send);
+
+        log.info("{} users has been notified", emails.length);
+
+        return new OutputValues();
     }
 
-    public record InputValues() implements UseCase.InputValues {
+    public record InputValues(PageInfo pageInfo) implements UseCase.InputValues {
     }
 
     public record OutputValues() implements UseCase.OutputValues {
